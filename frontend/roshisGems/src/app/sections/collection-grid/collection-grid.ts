47fee8bfd6dc25ddaa0645cb30a89faf925ff664
Swati-Vishwa <swativishwa2020@gmail.com>
2026-08-08 17:29:50 +0530

    Added collection section

--- frontend/roshisGems/src/app/sections/collection-grid/collection-grid.ts
+++ frontend/roshisGems/src/app/sections/collection-grid/collection-grid.ts
@@ -7,4 +7,39 @@ import { CollectionCard } from "../../shared/collection-card/collection-card";
   templateUrl: './collection-grid.html',
   styleUrl: './collection-grid.css',
 })
-export class CollectionGrid {}
+export class CollectionGrid {
+  collectionItem = [
+    {
+      id: 100,
+      image: "/images/img3.jpeg",
+      altText: "Stud Earrings",
+      title: "Stud Earrings",
+      detail: "Delicate details, lasting elegance.",
+      routeLink: "/",
+    },
+    {
+      id: 101,
+      image: "/images/img4.png",
+      altText: "Artisan Jhumkas",
+      title: "Artisan Jhumkas",
+      detail: " Delicate details, lasting elegance.",
+      routeLink: "/",
+    },
+    {
+      id: 102,
+      image: "/images/img5.png",
+      altText: "Statement Necklaces",
+      title: "Statement Necklaces",
+      detail: "Delicate details, lasting elegance.",
+      routeLink: "/",
+    },
+    {
+      id: 103,
+      image: "/images/bracelet.png",
+      altText: "Handcrafted Bracelets",
+      title: "Handcrafted Bracelets",
+      detail: "Delicate details, lasting elegance.",
+      routeLink: "/",
+    },
+  ]
+}
--- frontend/roshisGems/src/app/shared/collection-card/collection-card.ts
+++ frontend/roshisGems/src/app/shared/collection-card/collection-card.ts
@@ -1,9 +1,19 @@
-import { Component } from '@angular/core';
+import { Component, input } from '@angular/core';
+import { RouterLink } from "@angular/router";
+import { MatIconModule } from '@angular/material/icon'
 
 @Component({
   selector: 'app-collection-card',
-  imports: [],
+  imports: [RouterLink, MatIconModule],
   templateUrl: './collection-card.html',
   styleUrl: './collection-card.css',
 })
-export class CollectionCard {}
+export class CollectionCard {
+  collectionItems = input.required<{
+    image: string;
+    altText: string;
+    title: string;
+    detail: string;
+    routeLink: string;
+  }>()
+}
